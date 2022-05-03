ef29955dcc6d21d2e7beab8ff9decaad2210fb92
avi8400833379 <50071379+avi8400833379@users.noreply.github.com>
2022-05-04 01:08:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const {readFileSync} = require('fs');
+const handlebars = require('handlebars');
+const bunyan = require('bunyan');
+
+// Imports the Google Cloud client library for Bunyan
+const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
+
+// Creates a Bunyan Cloud Logging client
+const loggingBunyan = new LoggingBunyan();
+
+// Create a Bunyan logger that streams to Cloud Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
+const logger = bunyan.createLogger({
+  // The JSON payload of the log as it appears in Cloud Logging
+  // will contain "name": "my-service"
+  name: 'my-service',
+  streams: [
+    // Log to the console at 'info' and above
+    {stream: process.stdout, level: 'info'},
+    // And log to Cloud Logging, logging at 'info' and above
+    loggingBunyan.stream('info'),
+  ],
+});
+
+const app = express();
+// Serve the files in /assets at the URI /assets.
+app.use('/assets', express.static('assets'));
+
+// The HTML content is produced by rendering a handlebars template.
+// The template values are stored in global state for reuse.
+const data = {
+  service: process.env.K_SERVICE || '???',
+  revision: process.env.K_REVISION || '???',
+};
+let template;
+
+app.get('/', async (req, res) => {
+  logger.info(`Received request at ${new Date(Date.now()).toUTCString()}`);
+  // The handlebars template is stored in global state so this will only once.
+  if (!template) {
+    // Load Handlebars template from filesystem and compile for use.
+    try {
+      template = handlebars.compile(readFileSync('index.html.hbs', 'utf8'));
+    } catch (e) {
+      logger.error(e);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+
+  // Apply the template to the parameters to generate an HTML string.
+  try {
+    const output = template(data);
+    res.status(200).send(output);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Hello from Cloud Run! The container started successfully and is listening for HTTP requests on ${PORT}`
+  );
+});
